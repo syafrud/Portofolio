01d6812d90ede63351ae7e9d809542367e6e8906
syafrud <syafruddin0234@gmail.com>
2025-09-15 13:27:20 +0700

    fix config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? "/Portofolio" : "",
-  assetPrefix: isProd ? "/Portofolio/" : "",
+  basePath: "/Portofolio",
+  assetPrefix: "/Portofolio/",
   trailingSlash: true,
 };
 
