f664b92f7469c48ac84a02470327cc419a374686
syafrud <syafruddin0234@gmail.com>
2025-09-15 13:23:53 +0700

    fix github pages config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
-// import type { NextConfig } from "next";
-
-// next.config.js
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: "/Portofolio",
+  basePath: isProd ? "/Portofolio" : "",
+  assetPrefix: isProd ? "/Portofolio/" : "",
+  trailingSlash: true,
 };
 
 export default nextConfig;
