16a1c43ab879ab8b363806ad93def1fbeb88f253
syafrud <syafruddin0234@gmail.com>
2025-09-15 13:38:42 +0700

    fix config and yml

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  distDir: "dist",
   images: {
     unoptimized: true,
   },
-  basePath: "/Portofolio",
-  assetPrefix: "/Portofolio/",
-  trailingSlash: true,
+  // basePath: "/Portofolio",
+  // assetPrefix: "/Portofolio/",
+  // trailingSlash: true,
 };
 
 export default nextConfig;
